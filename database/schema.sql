1ed40d21e7f87d54407e5f5684722060db9cf5b9
mahela97 <m.pradeepd@gmail.com>
2021-01-24 18:03:13 +0530

    Final

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,21 @@
----------------------------------------------------------
--- Table structure for admin
+CREATE DATABASE TV2;
+USE TV2;
+
 CREATE TABLE `admin` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL
 );
-----------------------------------------------------------
+
 
 CREATE TABLE `channel` (
   `channel_id` varchar(20) NOT NULL,
   `channel_name` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `feedbacks`
---
+
+
 
 CREATE TABLE `feedbacks` (
   `user_id` int(10) DEFAULT NULL,
@@ -25,11 +24,9 @@ CREATE TABLE `feedbacks` (
   `program_id` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `programs`
---
+
+
 
 CREATE TABLE `programs` (
   `program_id` int(20) NOT NULL,
@@ -37,33 +34,25 @@ CREATE TABLE `programs` (
   `program_name` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `staff`
---
+
+
 
 CREATE TABLE `staff` (
   `user_id` int(10) NOT NULL,
   `channeld_id` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `stared_program`
---
+
 
 CREATE TABLE `stared_program` (
   `user_id` int(10) NOT NULL,
   `program_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `timeslot`
---
+
 
 
 CREATE TABLE `timeslot` (
@@ -72,34 +61,11 @@ CREATE TABLE `timeslot` (
   `end_time` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `timeslot`
---
 
 
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `timeslot`
---
 
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `timeslot`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE `user` (
   `user_id` int(10) NOT NULL,
@@ -116,23 +82,10 @@ CREATE TABLE `day` (
   `day` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `day`
---
 ALTER TABLE `day`
   ADD PRIMARY KEY (`day_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `day`
---
 ALTER TABLE `day`
   MODIFY `day_id` int(20) NOT NULL AUTO_INCREMENT;
 
@@ -153,30 +106,13 @@ CREATE TABLE `programtime` (
   `programtime_id` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `programtime`
---
 ALTER TABLE `programtime`
   ADD PRIMARY KEY (`programtime_id`),
   ADD KEY `program_id` (`program_id`),
   ADD KEY `day_id` (`day_id`),
   ADD KEY `timeslot_id` (`timeslot_id`);
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `programtime`
---
-
---
--- Indexes for dumped tables
---
 
 
 
@@ -184,50 +120,36 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY(`email`);
 
 
---
--- Indexes for table `channel`
---
+
 ALTER TABLE `channel`
   ADD PRIMARY KEY (`channel_id`);
   
 
---
--- Indexes for table `feedbacks`
---
+
 ALTER TABLE `feedbacks`
   ADD PRIMARY KEY (`feedback_id`),
   ADD KEY `program_id` (`program_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `programs`
---
+
 ALTER TABLE `programs`
   ADD PRIMARY KEY (`program_id`),
   ADD KEY `channel_id` (`channel_id`);
 
---
--- Indexes for table `staff`
---
 
 
---
--- Indexes for table `stared_program`
---
+
+
 ALTER TABLE `stared_program`
   ADD PRIMARY KEY (`user_id`,`program_id`),
   ADD KEY `program_id` (`program_id`);
 
---
--- Indexes for table `timeslot`
---
+
 ALTER TABLE `timeslot`
   ADD PRIMARY KEY (`timeslot_id`);
 ALTER TABLE `timeslot`
   MODIFY `timeslot_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
---
--- Indexes for table `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 ALTER TABLE `user` CHANGE `user_id` `user_id` INT(10)AUTO_INCREMENT;
@@ -241,36 +163,23 @@ ADD COLUMN `pet` VARCHAR(255) AFTER `type`;
 ALTER TABLE user ADD UNIQUE (email);
 
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `feedbacks`
---
 ALTER TABLE `feedbacks`
   ADD CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`program_id`),
   ADD CONSTRAINT `feedbacks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
---
--- Constraints for table `programs`
---
+
 ALTER TABLE `programs`
   ADD CONSTRAINT `programs_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`channel_id`);
   
 
---
--- Constraints for table `staff`
---
+
 ALTER TABLE `staff`
   ADD CONSTRAINT `staff_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`channeld_id`) REFERENCES `channel` (`channel_id`);
   
 
 
---
--- Constraints for table `stared_program`
---
 ALTER TABLE `stared_program`
   ADD CONSTRAINT `stared_program_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`program_id`),
   ADD CONSTRAINT `stared_program_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
